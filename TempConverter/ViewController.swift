182c9435aa3c60c5b44267908fa36f47b7a3c605
GGunta <msdoors@gmail.com>
2021-07-31 22:16:13 +0300

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by gunta.golde on 31/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
